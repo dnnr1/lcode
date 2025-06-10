a544b85203bafcbbcc73181fafebcd60ad0315fe
Daniel Roberto <sdanirobert@outlook.com>
2025-06-10 16:29:15 -0300

    majorityElement

---
+++ majorityElement.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let count = 0;
+  let candidate = null;
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    count += num === candidate ? +1 : -1;
+  }
+  return candidate;
+};
+
+console.log(majorityElement([3, 2, 3]));
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
