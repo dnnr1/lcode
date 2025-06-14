da326740ce4eb111cc577e82ffd9a59f812809f3
Daniel Roberto <sdanirobert@outlook.com>
2025-06-14 03:47:21 -0300

    removeDuplicates

---
+++ removeDuplicates.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  console.log(nums);
+  return i + 1;
+};
+
+console.log(removeDuplicates([1, 1, 2])); // output: 2
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // output: 5
